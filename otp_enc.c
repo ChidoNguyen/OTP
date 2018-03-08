4eaa5a4b71f9c6a22b95ebb689db29377375c609
Chido Nguyen <chidonguyen@hotmail.com>
2018-03-07 22:05:01 -0800

    Talking between daemon and client works
    -Buffer transfer is good to go
    -Implement cipher and return it to otp-enc

--- otp_enc.c
+++ otp_enc.c
@@ -12,6 +12,8 @@ our "client" to talk to our server daemon. Program takes 2 arguments our plainte
 #include <netinet/in.h>
 #include <netdb.h>
 
+char* pass = "shouldbeasecret";
+
 void error(const char *msg) { perror(msg); exit(0); } // Error function used for reporting issues
 
 int main(int argc, char* argv[]){
@@ -20,11 +22,12 @@ int main(int argc, char* argv[]){
 	struct sockaddr_in serverAddress;
 	struct hostent* serverHostInfo;
 	char buffer[500000];
-	
+	char buffer2[500000];
+	FILE* fp;
 	//add arg check later//
 	
 	memset((char*)&serverAddress, '\0', sizeof(serverAddress)); // clears
-	portNumber = atoi(argv[1]); // CHANGE THIS LATER !!!!!!!
+	portNumber = atoi(argv[3]); // CHANGE THIS LATER !!!!!!!
 	serverAddress.sin_family = AF_INET; // network capable socketFD
 	serverAddress.sin_port = htons(portNumber);// store port #
 	char *tmp = "localhost";
@@ -43,17 +46,53 @@ int main(int argc, char* argv[]){
 	if( connect ( socketFD, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) < 0) error("Client: Error Connecting");
 	
 	// Send stuff to server //
-	printf("Client: Test a phrase: ");
+	/* printf("Client: Test a phrase: ");
 	memset(buffer, '\0', sizeof(buffer));
 	fgets(buffer, sizeof(buffer)-1, stdin); // shorten buffer by 1 to have null term
-	buffer[strcspn(buffer, "\n")] = '\0'; // Remove the trailing \n that fgets adds
+	buffer[strcspn(buffer, "\n")] = '\0'; // Remove the trailing \n that fgets adds */
 	
 	// actual SEND //
-	charsWritten = send(socketFD, buffer, strlen(buffer), 0);
+	//charsWritten = send(socketFD, buffer, strlen(buffer), 0);
+	charsWritten = send(socketFD, pass, strlen(pass), 0);
 	if(charsWritten < 0 ) error("Client: Send failed");
-	if( charsWritten < strlen(buffer)) printf("Warning dropped packets\n");
+	if( charsWritten < strlen(pass)) printf("Warning dropped packets\n");
+	/////////////////
+	memset(buffer, '\0', sizeof(buffer));
+	memset(buffer2, '\0', sizeof(buffer));
+	fp= fopen(argv[1], "r");
+	int wordcount;
+	wordcount = 0;
+	char c;
+	c=getc(fp);
+	while(c != '\n'){
+		buffer[wordcount] = c;
+		wordcount++;
+		c = getc(fp);
+	}
+	fclose(fp);
 	
-	//server response //
+	fp= fopen(argv[2], "r");
+	int wordcount2;
+	wordcount2 = 0;
+	char d;
+	d=getc(fp);
+	while(d != '\n'){
+		buffer2[wordcount2] = d;
+		wordcount2++;
+		d= getc(fp);
+	}
+	fclose(fp);
+	
+	buffer[strcspn(buffer, "\n")] = '\0'; // Remove the trailing \n that fgets adds
+	buffer2[strcspn(buffer2, "\n")] = '\0'; // Remove the trailing \n that fgets adds
+
+	charsWritten = send(socketFD, buffer, strlen(buffer), 0);
+	memset(buffer, '\0', sizeof(buffer));
+	charsRead = recv(socketFD, buffer, sizeof(buffer) -1, 0);
+	if(charsRead < 0 ) error("Client: crickets in the socket");
+	printf("Client: Server sent - %s\n", buffer);
+	
+	charsWritten = send(socketFD, buffer2, strlen(buffer2), 0);
 	memset(buffer, '\0', sizeof(buffer));
 	charsRead = recv(socketFD, buffer, sizeof(buffer) -1, 0);
 	if(charsRead < 0 ) error("Client: crickets in the socket");
--- otp_enc_d.c
+++ otp_enc_d.c
@@ -11,15 +11,78 @@ This is our daemon or "server" that listens for connection requests from OTP_ENC
 #include <unistd.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+// "secret pass" to verify ID of which client is sending to it //
+char* pass = "shouldbeasecret";
+
+char* KEY[27] = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"," "};
+
 void error(const char *msg) { perror(msg); exit(1); } // Error function used for reporting issues
 
+struct history{
+	int arr[5];
+};
+struct history SPID;
+
+/*
+REAP_THE_SOULS
+Purpose: To clean up child forked processes
+Simple for loop iteration through global struct array, and does a 
+waitpid with WNOHANG
+*/
+void REAP_THE_SOULS(){
+	int z, childstatus;
+	waitpid(-1,0,WNOHANG);
+	 for(z = 0 ; z < 5; z++){
+		if(SPID.arr[z] != -1){
+			if(waitpid(SPID.arr[z], &childstatus, WNOHANG)){
+				if(WIFEXITED(childstatus)){
+					SPID.arr[z] = -1;
+				}
+			}
+		}
+	} 
+}
+
+/*
+handshake:
+checks to see if our passphrases match up prevents DEC from talking to our daemon
+Input: takes a string
+Output: 1-> for matches , 0 for all other cases
+*/
+int handshake(char *arr, int establishedConnectionFD){
+	int charsRead;
+	memset(arr, '\0', 20);
+	charsRead = recv(establishedConnectionFD,arr, 15, 0);
+	if(charsRead < 0)
+		fprintf(stderr, "Handshake empty");
+	if(strcmp( arr, pass) == 0)
+		return 1;
+	else
+		return 0;
+	
+}
+/*
+OTP_encrypt:
+This is our encryption function , following OTP mod 27 procedure.
+input: plaintext array , and key array of characters
+output: sends back a cipher text.
+*/
 int main( int argc, char * argv[]){
+	//
+	int z;
+	for(z = 0; z < 5; z++){
+		SPID.arr[z] = -1;
+	}
+	//
 	int listenSocketFD;
 	int establishedConnectionFD;
 	int portNumber;
 	int charsRead;
+	int gatekeeper;
 	socklen_t sizeOfClientInfo;
 	char buffer[500000];
+	char buffer2[500000];
+	char hand[20];
 	struct sockaddr_in serverAddress, clientAddress;
 	
 	// Check to make sure only 2 arguments are used total
@@ -40,20 +103,55 @@ int main( int argc, char * argv[]){
 		error("Binding of socket and port failed");
 	listen(listenSocketFD, 5); // turns socket on , 5 connections max
 	while(1){
+		REAP_THE_SOULS();
 		//Accepting connection requests , blocking if more than 5
 		sizeOfClientInfo = sizeof(clientAddress);
 		establishedConnectionFD = accept(listenSocketFD, (struct sockaddr *)&clientAddress, &sizeOfClientInfo);
 		if(establishedConnectionFD < 0 ) error("Could not accept connection appropriately");
 		printf("SERVER: Connected Client at port %d\n", ntohs(clientAddress.sin_port));
-		//Recv message and display 
-		memset(buffer, '\0', 500000);
-		charsRead = recv(establishedConnectionFD, buffer, 500000, 0);
-		if(charsRead < 0 ) error("Empty coms");
-		printf("# of chars: %i \nMessage: %s\n", charsRead, buffer);
-		// Replies ( send) to client
-		charsRead = send(establishedConnectionFD,"I am the server, and I got your message", 39, 0);
-		if(charsRead < 0 ) error("Send to sock failed");
-		close(establishedConnectionFD);
+		////////////////////TESTING FORK STUFF///////////////////////////
+		int spawnPID = fork();
+		for(z = 0; z < 5; z++){
+			if(SPID.arr[z] == -1){
+				SPID.arr[z] = spawnPID;
+				break;
+			}
+		}
+		if(spawnPID == -1){
+			printf("BROKEN\n");
+		}
+		else if(spawnPID == 0){
+			/*To-do:
+			"link" enc_d with enc only dont allow DEC to send to itoa
+			-after verifying we need to grab the sent files
+			-encrypt
+			-send back cipher text
+			*/
+			gatekeeper = handshake(hand,establishedConnectionFD);
+			
+			if(gatekeeper == 1){
+				memset(buffer, '\0', 500000);
+				memset(buffer2, '\0', 500000);
+				
+				charsRead = recv(establishedConnectionFD, buffer, 500000, 0);
+				if(charsRead < 0 ) error("Empty coms");
+				printf("# of chars: %i \nMessage: %s\n", charsRead, buffer);
+				// Replies ( send) to client
+				charsRead = send(establishedConnectionFD,"I am the server, and I got your message", 39, 0);
+				if(charsRead < 0 ) error("Send to sock failed");
+				
+				charsRead = recv(establishedConnectionFD, buffer2, 500000, 0);
+				if(charsRead < 0 ) error("Empty coms");
+				printf("# of chars: %i \nMessage: %s\n", charsRead, buffer2);
+				// Replies ( send) to client
+				charsRead = send(establishedConnectionFD,"I am the server, and I got your message", 39, 0);
+				if(charsRead < 0 ) error("Send to sock failed");
+			}
+			else
+				fprintf(stderr, "Unauthorized access");
+			close(establishedConnectionFD);
+			exit(0);
+		}
 	}
 	close(listenSocketFD);
 	return 0;
